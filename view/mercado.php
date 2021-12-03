d970c2b851f8cbfe9a69341564d2a02519ae466d
Filipe Sobral <filipe.nsv@gmail.com>
2021-12-03 09:23:41 -0300

    Paginas mercado e noticias

---
+++ view/mercado.php
@@ -0,0 +1,26 @@
+<?php
+
+    require_once("header.html");
+?>
+      <section id="secao">
+ 
+            <div>
+                <h2>MERCADO</h2>
+                
+            </div>
+
+            <div id="Products" class="container">
+
+                <div class="row">
+                    <div class="col-md-4"><a href="#"><img src="../img/img1.jpg" alt=""><br><br>Processador AMD Ryzen 5 5600G, 3.9GHz (4.4GHz Max Turbo), AM4, Vídeo Integrado, 6 Núcleos - 100-100000252BOX</a></div>
+                    <div class="col-md-4"><a href="#"><img src="../img/img2.jpg" alt=""><br><br>Nintendo Switch 32GB, 1x Joycon, Neon Azul/Vermelho - HBDSKABA1</a></div>
+                    <div class="col-md-4"><a href="#"><img src="../img/img3.jpg" alt=""><br><br>Headset Gamer Redragon Scylla, Drivers 40mm, P2 - H901</a></div>
+                </div>
+
+            </div>
+        </section>
+
+<?php
+    require_once("footer.html");
+
+?>
---
+++ view/noticias.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once("header.html");
+?>
+      <section id="secao">
+          
+            <div>
+                <h1>NOTÍCIAS</h1>
+                
+            </div>
+
+            <div id="news" class="container">
+
+                <div class="row">
+                    <div class="col-md-4"><a href="#"><img src="../img/ntc1.jpg" alt=""><br><br>Novo Snapdragon ultrapassa 1 milhão de pontos no AnTuTu</a></div>
+                    <div class="col-md-4"><a href="#"><img src="../img/ntc2.jpg" alt=""><br><br>6 consoles para comprar na Black Friday 2021</a></div>
+                    <div class="col-md-4"><a href="#"><img src="../img/ntc3.jpg" alt=""><br><br>5 streaming players para adquirir na Black Friday 2021</a></div>
+                </div>
+
+            </div>
+            
+        </section>
+
+<?php
+    require_once("footer.html");
+
+?>
