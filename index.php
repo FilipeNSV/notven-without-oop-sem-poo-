f428678a1527277e17c7b276142e194ebc6226ba
Filipe Sobral <filipe.nsv@gmail.com>
2022-01-28 14:37:41 -0300

    NotVen Atualizado

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 
                     <div class="float-sm-right">
                         <form class="form-inline" style="margin-right: 20px; display: flex;">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Busca..." aria-label="Search">
+                            <input id="search" class="form-control mr-sm-2" type="search" placeholder="Busca..." aria-label="Search">
                             <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                               </svg></button>
@@ -72,7 +72,7 @@
 
                         <div class="media">
 
-                            <img class="mr-3" src="view/img/logo.png" alt="Generic placeholder image" style="margin-top: 60px;">
+                            <img class="mr-3" src="view/img/logo.png" id="logo" alt="Generic placeholder image">
                             
                         </div>
                     </div>
